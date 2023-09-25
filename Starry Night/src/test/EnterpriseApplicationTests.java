9d068e23db19dcdaef9244b88b2b7d19a3968418
Kelli Porter <kelli.porter1998@gmail.com>
2023-09-25 10:33:03 -0400

    Added place holder Java methods.

---
+++ Starry Night/src/test/EnterpriseApplicationTests.java
@@ -0,0 +1,18 @@
+//--- Imports ---//
+
+//--- Summary ---//
+//This is where we perform our test drivin design
+
+//--- Application ---//
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+    //Test #1
+    //When the user opens the application, the app will display information on the users current location
+
+    //Test #2
+    //When the user enters coordinates, the app will display information in relation to those coordinates
+
+    //Test #3
+    //When a user wants to look at past locations, they will be able to view a saved location list.
+}
