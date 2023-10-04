7d2414b27b0d3e601604d4c781c07d5d9b9443b7
Kelli Porter <kelli.porter1998@gmail.com>
2023-10-04 10:06:42 -0400

    Fixed Dependencies in Project
    Had to recreate the project because there were missing dependencies.

---
+++ StaryNight/src/main/java/com/example/starynight/StaryNightApplication.java
@@ -0,0 +1,13 @@
+package com.example.starynight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaryNightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaryNightApplication.class, args);
+	}
+
+}
---
+++ StaryNight/src/test/java/com/example/starynight/StaryNightApplicationTests.java
@@ -0,0 +1,25 @@
+package com.example.starynight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaryNightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	//Test #1
+	//When the user opens the application, the app will display information on the users current location
+
+	//Test #2
+	//When the user enters coordinates, the app will display information in relation to those coordinates
+
+	//Test #3
+	//When the user wants to save a search, the app will add the entry to an arraylist
+
+	//Test #4
+	//When a user wants to look at past locations, they will be able to view a saved location list.
+
+}
