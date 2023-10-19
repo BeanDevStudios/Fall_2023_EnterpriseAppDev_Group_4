de572b7b448083bb6ed4913e8d9f4c6b359e731e
Ben Devine <devinebj@mail.uc.edu>
2023-10-19 17:13:40 -0400

    Added a starting homepage

--- StaryNight/src/main/java/com/example/starynight/StaryNightController.java
+++ StaryNight/src/main/java/com/example/starynight/StaryNightController.java
@@ -1,10 +1,12 @@
 package com.example.starynight;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class StaryNightController {
 
-    /*
+    /**
      * Handle the / endpoint
      * @return
      */
