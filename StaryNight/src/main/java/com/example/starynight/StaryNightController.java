f824db11f9ec3bb649560dd79eadb39e3d312891
David <torandc@mail.uc.edu>
2023-12-13 10:49:38 -0500

    Added method to save locations viewed

--- StaryNight/src/main/java/com/example/starynight/StaryNightController.java
+++ StaryNight/src/main/java/com/example/starynight/StaryNightController.java
@@ -1,15 +1,38 @@
 package com.example.starynight;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.starynight.dto.SaveData;
+import com.example.starynight.service.SavesService;
+
+@Controller
 public class StaryNightController {
+
+    @Autowired
+    private SavesService savesService;
 
     /*
      * Handle the / endpoint
      * @return
      */
+
     @RequestMapping("/")
-    public String index(){
-        return "start";
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/savedAreaInfo")
+    public ResponseEntity savedAreaInfo(SaveData data){
+        try{
+            savesService.save(data);
+            return new ResponseEntity(HttpStatus.OK);
+        }catch(Exception exception){
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
     }
 }
